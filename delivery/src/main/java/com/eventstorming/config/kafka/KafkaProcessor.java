8826470b518973a92559b4ce187e7f49370e4455
Jinyoung Jang <jyjang@uengine.org>
2022-12-14 17:29:48 +0900

    commit

---
+++ delivery/src/main/java/com/eventstorming/config/kafka/KafkaProcessor.java
@@ -0,0 +1,22 @@
+path: delivery/src/main/java/labshopcompensation/config/kafka
+---
+
+package labshopcompensation.config.kafka;
+
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
+
+public interface KafkaProcessor {
+
+    String INPUT = "event-in";
+    String OUTPUT = "event-out";
+
+    @Input(INPUT)
+    SubscribableChannel inboundTopic();
+
+    @Output(OUTPUT)
+    MessageChannel outboundTopic();
+
+}
